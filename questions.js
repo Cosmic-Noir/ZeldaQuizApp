e14d832a088116bcd29bdb14d06f7f4a8e1b7600
Kristen Doucette <cosmicnoir90@gmail.com>
2019-05-17 19:58:53 -0600

    Add array of questions.

---
+++ questions.js
@@ -0,0 +1,39 @@
+// Should store an array of objects, with each object a set of question properties and values, which we can reference. 
+
+let QUESTIONS = [
+    {
+        question: "Who is Zleda?",
+        answer: "The Princess of Hyrule.",
+        alt1: "The young hero dressed in green.",
+        alt2: "The fairy queen.",
+        alt3: "The evil demon king."
+    }, 
+    {
+        question: "When was Ocarina of Time originally released?",
+        answer: "November, 1998.",
+        alt1: "Octorber, 1993.",
+        alt2: "December, 1999.",
+        alt3: "December, 2003."
+    },
+    {
+        question: "What console was the title released on?",
+        answer: "Nintendo 64.",
+        alt1: "playstaion.",
+        alt2: "Super Nintendo.",
+        alt3: "Dreamcast."
+    },
+    {
+        question: "What is the name of the main character?",
+        answer: "Link.",
+        alt1: "Zelda.",
+        alt2: "Saori.",
+        alt3: "Epona."
+    },
+    {
+        question: "What is the name of hero's fairy?",
+        answer: "Navi.",
+        alt1: "Epona.",
+        alt2: "Theo.",
+        alt3: "Malta."
+    }
+]
