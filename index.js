8c2ceb1ab86eb6b1d4f1d9672e6fec3707d9070d
Kristen Doucette <cosmicnoir90@gmail.com>
2019-05-18 19:38:13 -0600

    Fix question counter and score counter.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let score = 0;
 let questionIndex = 0;
+let questionDisplay = 1;
+
 
 function handleBegin() {
     // responsible for hiding the "intro" class section when the "begin" button is clicked. 
@@ -15,7 +17,8 @@ function handleBegin() {
 
 function renderQuestion() {
     // This function will be responsible for rendering the question and adding it to the "question" class section
-    let newSection = `<section class="question">
+    if (questionIndex < QUESTIONS.length) {
+        let newSection = `<section class="question">
         <h2 class="title">${QUESTIONS[questionIndex].question}</h2>
         <form>
             <input type="radio" class="choice" name="choice" value="${QUESTIONS[questionIndex].answers[0]}" required>${QUESTIONS[questionIndex].answers[0]}<br>
@@ -26,8 +29,13 @@ function renderQuestion() {
         </form>
     </section>`;
     $('.container').append(newSection);
-    console.log('`renderQuestion` ran');
+    console.log('`renderQuestion` ran and created a new question.');
     handleSubmit();
+    } else {
+        console.log('`renderQuestion` rand and there were no more questions. Finishing quiz...');
+        handleFinish();
+    }
+    
     
 };
 
@@ -35,30 +43,34 @@ function handleSubmit() {
     // function to handle all functions when the "submit answer" button is clicked
     $('#submitIt').click(function(event) {
         event.preventDefault();
+        console.log('`handleSubmit` ran');
         handleAnswer();
-        // removeSubContainer();
-        hanldeQuestNav(); // May want to move
         $('.question').remove();
+        
     });    
-    console.log('`handleSubmit` ran');
+   
 }
 
 function handleAnswer() {
     // this function is responsible for deciding if the selected answer value is equal to the correct answer. 
     let selectedAnswer = $('input[name="choice"]:checked').val();
-    console.log(selectedAnswer);
+    
     if (selectedAnswer === QUESTIONS[questionIndex].correct){
+        console.log('A matching answer was found with the value of ' + selectedAnswer);
+        console.log('`handleAnswer` ran');
         handleCorrect();
     } else {
+        console.log('`handleAnswer` ran');
         handleWrong();
     }
-    console.log('`handleAnswer` ran');
 }
 
 function hanldeQuestNav() {
     // function responsible for updating what question number the user is on. 
     questionIndex++;
-    $('#questNumb').text(questionIndex);
+    questionDisplay++;
+    console.log(questionDisplay);
+    $('#questNumb').text(questionDisplay);
     console.log('`handleQuestNav` ran and questionIndex is equal to ' + questionIndex);
 
     
@@ -68,23 +80,28 @@ function handleCorrect() {
     // function responsible for showing the user selected the correct answer.
     score++;
     $('#score').text(score);
-    displayCorrect();
     console.log('`handleCorrect` ran and the score is now ' + score);
+    displayCorrect();
+    
 
 }
 
 function handleWrong() {
     // function responsible for showing the user they selected the wrong anser.
+    handleNext();
+    console.log('`handleWrong` ran');
+}
+
+function displayWrong() {
     let wrongDisplay = `<section class="subContainer">
     <h2 class="title">Sadness... A failure. But you must go on!!</h2>
     <button id="next">Next!</button>
-    </section>
     <h3>The correct answer was:</h3>
-    <h4>${QUESTIONS[questionIndex].correct}<h4>`;
-    $('.container').append(wrongDisplay);
-    console.log('`handleWrong` ran');
-    handleNext();
+    <h4>${QUESTIONS[questionIndex].correct}<h4>
+    </section>`;
     
+    $('.container').append(wrongDisplay);
+    console.log('`displayWrong` ran');
 }
 
 function displayCorrect(){
@@ -102,19 +119,29 @@ function handleNext(){
     // function responsible for when user clicks "next" button, will call hanldeQuestNav() to update questionIndex and re-call the question generator. 
     $('#next').click(function(event){
         event.preventDefault();
-        $('.subContainer').remove();
         hanldeQuestNav();
+        $('.subContainer').remove();
         renderQuestion();
         removeSubContainer();
-    });
 
     console.log('`handleNext` ran');
+    });
+
 }
 
 function removeSubContainer() {
     // responsible for removing any element with a class of subContainer
     $('.subContainer').remove();
     console.log('`removeSubContainer` ran');
+}
+
+function handleFinish() {
+    // function hanldes when there are no more questions in the QUESTIONS array.
+    let finishSection = `<section class="subContainer">
+    <h2 class="title">Congratulations! You've gained a heart for your wisdom!</h2>
+    <button id="restart">Restart!</button>
+    </section>`;
+    $('.container').append(finishSection);
 }
 
 function hanldeRestart() {
